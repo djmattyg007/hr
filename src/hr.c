a24fb31cc0b6d7f2082a3c8881acb2465a78a596
Matthew Gamble <git@matthewgamble.net>
2016-10-26 20:56:02 +1100

    Basic cleanup and optimisation

--- src/hr.c
+++ src/hr.c
@@ -1,30 +1,25 @@
 #include <stdio.h>
-#include <string.h>
 #include <sys/ioctl.h>
 #include <stdlib.h>
 
-int printline(int columns) {
-    for (int i = 0; i < columns; i++) {
-        printf("#");
-    }
-    printf("\n");
-    return 0;
-}
-
-int main(int argc, char * argv [])
+int main(int argc, char *argv [])
 {
     struct winsize w;
     ioctl(0, TIOCGWINSZ, &w);
-    int COLS = (w.ws_col <= 0 ? 80 : w.ws_col);
+    int COLS = (w.ws_col > 0 ? w.ws_col : 80);
     int lines = 1;
-
     if (argc > 1) {
         lines = atoi(argv[1]);
     }
+
+    char outchar[COLS];
+    for (int i = 0; i < COLS; i++) {
+        outchar[i] = '#';
+    }
+
     for (int x = 1; x <= lines; x++) {
-        printline(COLS);
+        printf("%s\n", outchar);
     }
 
     return 0;
 }
-
